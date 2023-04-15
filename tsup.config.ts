9fe12434d9f33be6c23e745e5a29fe5aac3c6eab
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2023-04-15 11:09:07 +0530

    First commit

---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+   entry: ['src/index.ts'],
+   splitting: true,
+   sourcemap: true,
+   clean: true,
+   dts: true,
+   format: ["cjs", "esm", "iife"],
+   minify: true,
+   outDir: "dist",
+   treeshake: true,
+   jsxFragment: "React.Fragment",
+   legacyOutput: true,
+   tsconfig: "./tsconfig.json",
+})
